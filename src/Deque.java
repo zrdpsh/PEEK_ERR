391c6a2dccf99bf7067f23e20722527f202523cb
zrdpsh <zerdposh@gmail.com>
2024-11-21 23:01:47 +0300

    dequeue with updated

--- src/Deque.java
+++ src/Deque.java
@@ -3,11 +3,16 @@ import java.util.LinkedList;
 
 public  class Deque<T> extends DequeAbs<T> {
 
+    public final int ENQUEUE_STATUS_NIL= 0; // dequeue method has run correctly
+    public final int ENQUEUE_STATUS_OK = 1; // dequeue method has run correctly
+    public final int ENQUEUE_STATUS_ERR = 2; // dequeue method has run correctly
+
     public final int DEQUEUE_STATUS_NIL= 0; // dequeue method has run correctly
     public final int DEQUEUE_STATUS_OK = 1; // dequeue method has run correctly
     public final int DEQUEUE_STATUS_ERR = 2; // dequeue method has run correctly
 
     private int dequeue_status;
+    private int enqueue_status;
 
     protected T item;
     protected LinkedList<T> linkedList;
@@ -15,6 +20,7 @@ public  class Deque<T> extends DequeAbs<T> {
     public Deque() {
         super();
         dequeue_status = DEQUEUE_STATUS_NIL;
+        enqueue_status = ENQUEUE_STATUS_NIL;
     }
 
     @Override
--- src/InitialQueue.java
+++ src/InitialQueue.java
@@ -3,21 +3,28 @@ import java.util.LinkedList;
 
 public class InitialQueue<T> extends InitialQueueAbs<T> {
 
-    public final int DEQUEUE_NIL = 0; // removeFront hasn't been evoked yet
-    public final int DEQUEUE_OK = 1; // removeFront method has run correctly
-    public final int DEQUEUE_ERR = 2; // removeFront method hasn't run correctly
+    public final int ENQUEUE_STATUS_NIL= 0; // enqueue method has run correctly
+    public final int ENQUEUE_STATUS_OK = 1; // enqueue method has run correctly
+    public final int ENQUEUE_STATUS_ERR = 2; // enqueue method has run correctly
+
+    public final int DEQUEUE_STATUS_NIL = 0; // removeFront hasn't been evoked yet
+    public final int DEQUEUE_STATUS_OK = 1; // removeFront method has run correctly
+    public final int DEQUEUE_STATUS_ERR = 2; // removeFront method hasn't run correctly
 
     public final int GET_ITEM_NIL = 0; // getItem hasn't been evoked yet
     public final int GET_ITEM_OK = 1; // getItem method has run correctly
+    public final int GET_ITEM_ERR = 2; // getItem method has run correctly
 
     private int dequeue_status;
+    private int enqueue_status;
     private int getStatus;
     protected T item;
     protected LinkedList<T> linkedList;
 
     public InitialQueue() {
         linkedList = new LinkedList<>();
-        dequeue_status = DEQUEUE_NIL;
+        dequeue_status = DEQUEUE_STATUS_NIL;
+        enqueue_status = ENQUEUE_STATUS_NIL;
         getStatus = GET_ITEM_NIL;
     }
 
@@ -30,16 +37,20 @@ public class InitialQueue<T> extends InitialQueueAbs<T> {
     public void dequeue() {
         item = linkedList.pollFirst();
         if (item != null) {
-            dequeue_status = DEQUEUE_OK;
+            dequeue_status = DEQUEUE_STATUS_OK;
           return;
         }
-        dequeue_status = DEQUEUE_ERR;
+        dequeue_status = DEQUEUE_STATUS_ERR;
     }
 
     @Override
     public T getItem() {
-        getStatus = GET_ITEM_OK;
-        return item;
+        if (linkedList.size() != 0) {
+            getStatus = GET_ITEM_OK;
+            return item;
+        }
+        getStatus = GET_ITEM_ERR;
+        return null;
     }
 
     @Override
